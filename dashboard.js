4fb9ca898bf060b84888a992f1206fdddeeb01fb
Kaveri <77133605+kavericodes@users.noreply.github.com>
2022-01-23 13:40:00 -0600

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,16 @@
+
+function back(){
+    window.location = "index.html";
+}
+
+function messaging(){
+    window.location = "messaging.html";
+}
+
+function job_search(){
+    window.location="search.html";
+}
+
+function profile_page(){
+    window.location="profile.html";
+}
